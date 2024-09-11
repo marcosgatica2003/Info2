c82c9219fe96acc7df5777895569b1db12a99bae
marcosgatica2003 <marcosgatica1112@gmail.com>
2024-09-11 13:47:54 -0300

    Ejercicio 4 de la guia de Arduino

--- guiaArduino/Arduino/util/util.ino
+++ guiaArduino/Arduino/util/util.ino
@@ -9,17 +9,23 @@ const int botonUno = 2;
 const int botonDos = 3;
 const int botonTres = 4;
 
+const int led1 = 8;
+const int led2= 9;
+const int led3= 10;
+
 int estadoUno, estadoDos, estadoTres;
 unsigned int enteroSerie = 0;
 
 void setup() {
   Serial.begin(9600);
 //pinMode(13, OUTPUT); //Ejercicio 1-2
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+
   pinMode(botonUno, INPUT_PULLUP);
   pinMode(botonDos, INPUT_PULLUP);
   pinMode(botonTres, INPUT_PULLUP);
-  Serial.begin(1200);
-  pinMode(13, OUTPUT);
   
  }
 
@@ -40,6 +46,38 @@ void loop() {
   enteroSerie |= (estadoUno == LOW) << 0;
   enteroSerie |= (estadoDos == LOW) << 1;
   enteroSerie |= (estadoTres == LOW) << 2;
+
+  if (Serial.available()){ 
+    Serial.read();
+    enteroSerie = 0;
+  	digitalWrite(led1, 1);
+    digitalWrite(led2, 1);
+    digitalWrite(led3, 1);
+    delay(1000);
+ 	digitalToggle(led1);
+    digitalToggle(led2);
+    digitalToggle(led3);
+    Serial.println("Entero puesto en 0.");
+  }
+    
+  if (enteroSerie == 1){
+	digitalWrite(led1, 1);
+    digitalWrite(led2, 0);
+    digitalWrite(led3, 0);
+  }
+  
+  if (enteroSerie == 2) {
+  	digitalWrite(led1, 0);
+    digitalWrite(led2, 1);
+    digitalWrite(led3, 0);
+  }
+  
+    if (enteroSerie == 4) {
+  	digitalWrite(led1, 0);
+    digitalWrite(led2, 0);
+    digitalWrite(led3, 1);
+  }
+
 
   Serial.println(enteroSerie);
   delay(demoraDefault);
